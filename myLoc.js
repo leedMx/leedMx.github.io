0d8def8bc9eefa1239209767f817abc6927e852f
leedMx <esparza.mexico@gmail.com>
2024-12-26 21:38:04 -0600

    Including distance computation

--- myLoc.js
+++ myLoc.js
@@ -12,4 +12,30 @@ function displayLocation(position) {
     var longitude = position.coords.longitude;
     var div = document.getElementById("location");
     div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
-   }
+
+    var km = computeDistance(position.coords, ourCoords);
+    var distance = document.getElementById("distance");
+    distance.innerHTML = "You are " + km + " km from the WickedlySmart HQ";
+}
+
+var ourCoords = {
+    latitude: 47.624851,
+    longitude: -122.52099
+};
+
+function computeDistance(startCoords, destCoords) {
+    var startLatRads = degreesToRadians(startCoords.latitude);
+    var startLongRads = degreesToRadians(startCoords.longitude);
+    var destLatRads = degreesToRadians(destCoords.latitude);
+    var destLongRads = degreesToRadians(destCoords.longitude);
+    var Radius = 6371; // radius of the Earth in km
+    var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +
+        Math.cos(startLatRads) * Math.cos(destLatRads) *
+        Math.cos(startLongRads - destLongRads)) * Radius;
+    return distance;
+}
+
+function degreesToRadians(degrees) {
+    var radians = (degrees * Math.PI) / 180;
+    return radians;
+}
